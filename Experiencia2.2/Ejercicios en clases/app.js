73fc82621f47e8c967e994182f14a3fd771021f9
Javier Solar <114122506+xKakashixpy@users.noreply.github.com>
2023-04-15 23:20:30 -0400

    Primera Parte

--- Experiencia2.2/Ejercicios en clases/app.js
+++ Experiencia2.2/Ejercicios en clases/app.js
@@ -1,6 +1,7 @@
 var nombre = document.getElementById('nombre');
 var password = document.getElementById('password');
 var error = document.getElementById('error');
+var edad = document.getElementById ('edad');
 error.style.color = 'Green';
 
 function enviarformulario(){
@@ -14,7 +15,10 @@ function enviarformulario(){
     if (password.value === null || password.value === ''){
         mensajeError.push('Ingresa tu password...'); 
     }
-
+    if  (edad.value < 18 || edad > 35) {
+        mensajeError.push("La edad debe estar entre 18 y 35 años.");
+        return false;
+    }
     error.innerHTML = mensajeError.join(',');
 
     return false; 
